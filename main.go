4cb5cc1a35baed86a6cdcad69852e32802c45ca9
Jean-Philippe Roemer <jp@roemer.im>
2016-04-20 04:15:00 +0200

    Trying to fix panic on incoming webhook
    - Webhook documentation says that head should show new HEAD
    - While in reality its the after field that give you the new HEAD

--- main.go
+++ main.go
@@ -159,8 +159,8 @@ func treatPushEvent(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	fmt.Println(*event.Head)
-	oid, _ := git.NewOid(*event.Head)
+	fmt.Println(*event.After)
+	oid, _ := git.NewOid(*event.After)
 	commit, err := gitRepo.LookupCommit(oid)
 	if err != nil {
 		log.WithError(err).Error("Couldn't get HEAD's commit")
